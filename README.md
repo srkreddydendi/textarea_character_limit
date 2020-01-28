e760a4630a6b8c73783ef97dbf59a770b837c1f4
srkreddydendi <srkreddydendi@gmail.com>
2020-01-28 13:15:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ant7cb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ant7cb)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('textarea');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
